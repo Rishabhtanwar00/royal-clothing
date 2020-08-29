43aafd89a5a886b5f596c3a64b07f62ca7c45d76
Rishabh Tanwar <rishabhtanwar2000@gmail.com>
2020-08-29 15:06:16 +0530

    adding redux and userReducer and Useractions to header and app component

--- src/App.js
+++ src/App.js
@@ -14,34 +14,30 @@ import { Route,Switch } from 'react-router-dom';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
+import { connect } from 'react-redux';
+
+import { setCurrentUser } from './redux/user/user.actions';
+
 
 class App extends React.Component {
-  constructor(){
-    super();
-
-    this.state={
-      currentUser:null
-    }
-  }
 
   unsubscribeFromAuth =null;
 
   componentDidMount(){
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth =>{
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot =>{
-          this.setState({
-            currentUser:{
+          setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
-            }
           });
         });
       }
       else{
-      this.setState({ currentUser: userAuth });
+      setCurrentUser({ userAuth });
       }
     });
   }
@@ -53,7 +49,7 @@ class App extends React.Component {
   render(){
     return (
       <div>
-        <Header currentUser={ this.state.currentUser }/>
+        <Header/>
         <Switch>
           <Route exact path='/' component={ HomePage }/>
           <Route path='/shop' component={ ShopPage }/>
@@ -65,4 +61,8 @@ class App extends React.Component {
 
 }
 
-export default App;
+const mapDispatchToProps = dispatch =>({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null,mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,6 +6,8 @@ import { ReactComponent as Logo} from '../../assets/crown.svg';
 
 import { auth } from '../../firebase/firebase.utils';
 
+import { connect } from 'react-redux';
+
 import './header.styles.scss';
 
 const  Header = ({ currentUser }) => (
@@ -27,4 +29,8 @@ const  Header = ({ currentUser }) => (
     </div>
 )
 
-export default Header;
+const mapStateToProps = state =>({
+    currentUser:state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
