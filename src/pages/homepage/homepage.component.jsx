33d953cd4660c3748cd3bbe534126e651cee4ab0
Rishabh Tanwar <rishabhtanwar2000@gmail.com>
2020-08-31 11:59:32 +0530

    adding shopReducer and more optmization in our react app

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 import './homepage.styles.scss';
 
-import Menu from '../../components/menu/menu.component';
+import Directory from '../../components/directory/directory.component';
 
 const HomePage = () =>(
     <div className="homepage">
-        <Menu />
+        <Directory />
     </div>
 )
 
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,26 @@
 import React from 'react';
 
-import SHOP_DATA from './shop.data.js';
+import { connect } from 'react-redux';
+
+import { createStructuredSelector } from 'reselect';
+
+import { selectColletions } from  '../../redux/shop/shop.selectors';
 
 import CollectionPreview from '../../components/Collection-Preview/collection-preview.component';
 
-class ShopPage extends React.Component{
-    constructor(props){
-        super(props);
-
-        this.state={
-            collections: SHOP_DATA  
-        };
-    }
-
-    render(){
-         const {collections} =this.state;
-        return (<div>
+const ShopPage = ({ collections }) =>(
+        <div className='shop-page'>
             {
                 collections.map(({id, ...otherCollectionProps}) =>(
                 <CollectionPreview key={id} {...otherCollectionProps}/>
             )
             )}
-        </div>)
-    }
-}
+        </div>
+);
 
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+    collections: selectColletions
+})
+
+
+export default connect(mapStateToProps)(ShopPage);
