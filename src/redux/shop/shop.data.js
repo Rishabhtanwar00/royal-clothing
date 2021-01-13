0088210262bfaa3666de5ac2a755ade7e6e67433
Rishabh Tanwar <rishabhtanwar2000@gmail.com>
2021-01-13 14:09:13 +0530

    .

--- src/redux/shop/shop.data.js
+++ src/redux/shop/shop.data.js
@@ -152,7 +152,7 @@ const SHOP_DATA = {
       },
     ],
   },
-  womens: {
+  women: {
     id: 4,
     title: "Womens",
     routeName: "womens",
@@ -201,7 +201,7 @@ const SHOP_DATA = {
       },
     ],
   },
-  mens: {
+  men: {
     id: 5,
     title: "Mens",
     routeName: "mens",
